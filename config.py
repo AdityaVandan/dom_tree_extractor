0e835e23c369328a2057a8735b741df742a868d4
Aditya Vandan Sharma <35309453+AdityaVandan@users.noreply.github.com>
2020-08-30 18:10:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 from selenium.webdriver.chrome.options import Options
 
 class driver_config:
-    driver_path = 'C:\\coding\\chrome_drivers\\cd_84\\chromedriver.exe'
+    driver_path = 'C:\\coding\\chrome_drivers\\cd_84\\chromedriver.exe' # add chrome driver path here
     options = Options()
     options.add_argument("--window-size=1920,1080")
     options.add_argument('--headless')
     options.add_argument('--incognito')
 
 class js_code:
-    js_script_path = 'C:\\coding\\load_js_methods.js'
+    js_script_path = '.\\load_js_methods.js'
     js_script = open(js_script_path).read()
